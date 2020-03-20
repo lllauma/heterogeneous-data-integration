a29c62a12eb286b0474d31d2f81a018a1ad3b13c
lllauma <lauma.svilpe@gmail.com>
2020-03-20 14:12:21 +0200

    Insert statements for filling tables
    Added insert statements for filling db tables (file name prefix indicates the sequence of scripts to be executeed to avoid constraint violance).

---
+++ db dml/1_insert_user_tab.sql
@@ -0,0 +1,11 @@
+INSERT INTO user_tab (
+    us_id,
+    us_name,
+    us_email,
+    us_password
+) VALUES (
+    1,
+    'Darja Solodovnikova',
+    'darja.solodovnikova@lu.lv',
+    null
+);
---
+++ db dml/4_insert_datahighwaylevel.sql
@@ -0,0 +1,7 @@
+REM INSERTING into DATAHIGHWAYLEVEL
+SET DEFINE OFF;
+Insert into DATAHIGHWAYLEVEL (HL_ID,HL_NAME,HL_CREATED,HL_DELETED) values (2,'Raw data level',to_date('14-FEB-20','DD-MON-RR'),null);
+Insert into DATAHIGHWAYLEVEL (HL_ID,HL_NAME,HL_CREATED,HL_DELETED) values (3,'abc',to_date('14-FEB-20','DD-MON-RR'),to_date('16-MAR-20','DD-MON-RR'));
+Insert into DATAHIGHWAYLEVEL (HL_ID,HL_NAME,HL_CREATED,HL_DELETED) values (5,'Structured data',to_date('16-MAR-20','DD-MON-RR'),null);
+Insert into DATAHIGHWAYLEVEL (HL_ID,HL_NAME,HL_CREATED,HL_DELETED) values (4,'tratata',to_date('15-FEB-20','DD-MON-RR'),to_date('16-MAR-20','DD-MON-RR'));
+Insert into DATAHIGHWAYLEVEL (HL_ID,HL_NAME,HL_CREATED,HL_DELETED) values (6,'Data warehouse',to_date('16-MAR-20','DD-MON-RR'),null);
